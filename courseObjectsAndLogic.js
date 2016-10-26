11fcd237e0f8445d8aca72dd6db853874c8fcc4a
LReller <hoth972@gmail.com>
2016-10-26 16:42:13 -0500

    Add files via upload

---
+++ courseObjectsAndLogic.js
@@ -0,0 +1,82 @@
+console.log("Start objects and logic");
+
+$(document).ready(function(){
+    //**********************DECLARATIONS OF OBJECTS**********************
+
+    //Data structure to hold topic area information
+    function TopicArea(name, credits)
+    {
+       this.name = name;
+       this.credits = credits;
+    }
+    
+    //Data structure to hold course info
+    //function Course(courseID, courseDepartment, courseNumber, courseName, credits, preReqs, coReqs, available, completion){
+    function Course(name, credits, available, completion){
+	   this.name = name;
+	   this.credits = credits;
+	   this.available = available;
+       this.completion = completion;
+    }
+    
+    //Create topicArea objects
+    var degree = new TopicArea("Software Engineering", 122);
+    var genEd = new TopicArea("General Education", 22);
+    var CSCI = new TopicArea("CSCI", 17);
+    var SEcore = new TopicArea("Software Engineering Core", 41);
+    var SEpath = new TopicArea("Software Engineering Electives", 9);
+
+    var commNode = new Course("Communication", 3, true, false);
+    var mathReq = new Course("Math Required", 14, true, false);
+    var mathEle = new Course("Math Electives", 6, true, false);
+    var sciReq = new Course("Science Required", 7, true, false);
+    var sciEle = new Course("Science Electives", 3, true, false);
+
+    //create individual course objects
+    //CSCI
+    var CSCI201 = new Course("CSCI 201", 3, true, false);
+
+
+    //*******************************************************************
+
+    
+    //*************************Population of HTML************************
+    
+    function populateHTML()
+    {
+        $("#totalCount span").text(degree.credits);
+        $("#CSCI div span").text(CSCI.credits);
+        $("#SEcore div span").text(SEcore.credits);
+        $("#SEpath div span").text(SEpath.credits);
+    }
+    
+    populateHTML();//initial population of HTML
+    
+    //*******************************************************************
+
+
+    //********************OBJECT LOGIC AND FUNCTIONS*********************
+
+    //Hides CSCI201 container upon completion
+    if(CSCI201.completion == true)
+    {
+        $("#CSCI201").hide();
+
+        CSCI.credits -= CSCI201.credits;
+        degree.credits -= CSCI201.credits;
+    }
+    
+    if(CSCI.credits == 0)
+    {
+        $("#CSCI").hide();
+    }
+    
+    populateHTML();//repopulate HTML to reflect changes
+    
+});
+
+
+
+
+
+
---
+++ jQuery.js
@@ -0,0 +1,41 @@
+console.log("Start jQuery.js");
+$(document).ready(function(){
+
+//expands LibEd course list
+	$('#genEd').click(function(){
+		if($('#genEdsub').css('display') == 'none')
+			$('#genEdsub').css('display', 'block');
+		else
+			$('#genEdsub').css('display', 'none');
+	});
+
+//expands CSCI course list
+	$('#CSCI').click(function(){
+		if($('#CSCIsub').css('display') == 'none')
+			$('#CSCIsub').css('display', 'block');
+		else
+			$('#CSCIsub').css('display', 'none');
+	});
+		
+//expands SEcore course list
+	$('#SEcore').click(function(){
+		if($('#SEcoresub').css('display') == 'none')
+			$('#SEcoresub').css('display', 'block');
+		else
+			$('#SEcoresub').css('display', 'none');
+	});
+		
+//expands SEpath course list
+	$('#SEpath').click(function(){
+		if($('#SEpathsub').css('display') == 'none')
+			$('#SEpathsub').css('display', 'block');
+		else
+			$('#SEpathsub').css('display', 'none');
+	});
+
+	
+	
+	
+	
+	
+});//end doc ready
